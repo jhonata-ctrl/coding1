0430642df3823e714f329d61be5bf9caf81d7d09
jhonata-ctrl <jhonatasilvajrt@gmail.com>
2024-09-25 16:24:16 -0300

    update

---
+++ aula1.js
@@ -0,0 +1,30 @@
+//codigo de teste
+/*
+var rotulo = "jhonata"
+var  rotulo = 12345
+console.log (rotulo)
+
+console.log ('hello' +' '+'world')
+*/
+
+/*
+const numero1 = 10;
+const numero2 = 70; 
+const soma = numero1 + numero2 
+console.log ("resultado "+ soma)
+
+const numero3 = 69
+const numero4 = 0
+const subtracao = numero3 - numero4
+console.log ("resultado "+ subtracao)
+
+const numero5 = 20
+const numero6 = 2
+const divisao = numero5 / numero6
+console.log ("resultado "+ divisao)
+
+const numero7 = 10
+const numero8 = 100
+const mult = numero7 * numero8 
+console.log ("resultado "+ mult)
+*/
